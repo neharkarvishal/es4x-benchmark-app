88435e80e530fb9f7b811225b537a62987765fbc
Vishal <neharkar.vishal@gmail.com>
2019-09-23 02:42:32 +0530

    Exiting ungracefully when port is unavailable. See FIXME in index.js Line 46.

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 vertx
   .createHttpServer()
   .requestHandler(app)
-  .listen(42069);
-
-console.log('Server listening at: http://localhost:42069/');
+  .listen(port, host, listen => {
+    if (listen.failed()) {
+      console.log('Failed to bind!');
+      // FIXME: `process.exit(1)` will terminate process silently and there will be no chance to recover from this
+      process.exit(1);
+    }
+    else {
+      console.log(`🚀 Server listening at http://${host}:${port}`);
+    }
+  }
+);
